89e987ac32fb886adcdd96fe0daacf961c7564e9
Dima Olaba <defire04st@gmail.com>
2023-02-13 15:10:54 +0200

    update/model user

--- src/main/java/com/example/dutpractice1/models/User.java
+++ src/main/java/com/example/dutpractice1/models/User.java
@@ -2,6 +2,10 @@ package com.example.dutpractice1.models;
 
 import jakarta.persistence.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "User")
@@ -10,4 +14,61 @@ public class User {
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "username")
+    @NotEmpty(message = "Name should be not empty!")
+    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
+    private String username;
+
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Age should be greater then 0!")
+    private int age;
+
+
+    @Column(name = "email")
+    @Email
+    @NotEmpty(message = "Email should be not empty!")
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, int age, String email) {
+        this.username = username;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
