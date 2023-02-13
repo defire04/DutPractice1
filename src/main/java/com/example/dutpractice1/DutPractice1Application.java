3d534c314ba7d28b2c5054785eaaad9967a6be25
Dima Olaba <defire04st@gmail.com>
2023-02-13 14:17:12 +0200

    Initial commit

---
+++ src/main/java/com/example/dutpractice1/DutPractice1Application.java
@@ -0,0 +1,13 @@
+package com.example.dutpractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutPractice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dutpractice1/DutPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dutpractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
