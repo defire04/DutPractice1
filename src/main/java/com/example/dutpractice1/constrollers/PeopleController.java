fcfc6d6e58dfa282c7492a2fb7c934a61ab5afb2
Dima Olaba <defire04st@gmail.com>
2023-02-14 12:53:37 +0200

    update/PersonDTO and create convertToPersonDTO in PeopleController

--- src/main/java/com/example/dutpractice1/constrollers/PeopleController.java
+++ src/main/java/com/example/dutpractice1/constrollers/PeopleController.java
@@ -1,30 +1,33 @@
 package com.example.dutpractice1.constrollers;
 
+import com.example.dutpractice1.dto.PersonDTO;
 import com.example.dutpractice1.models.Person;
 import com.example.dutpractice1.security.PersonDetails;
 import com.example.dutpractice1.services.PeopleService;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
 @RequestMapping()
-public class PersonController {
+public class PeopleController {
     public final PeopleService peopleService;
 
-    public PersonController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService) {
         this.peopleService = peopleService;
     }
 
     @GetMapping("/users")
-    public List<Person> showAll() {
-        return peopleService.findAll();
+    public List<PersonDTO> showAll() {
+        return peopleService.findAll().stream().map(this::convertToPersonDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/userInfo")
@@ -49,10 +52,14 @@ public class PersonController {
         return getCurrentUser().getUsername() + " you are admin!";
     }
 
-    public Person getCurrentUser() {
+    private Person getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
         return personDetails.getPerson();
     }
 
+    private PersonDTO convertToPersonDTO(Person person){
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(person, PersonDTO.class);
+    }
 }
--- src/main/java/com/example/dutpractice1/dto/PersonDTO.java
+++ src/main/java/com/example/dutpractice1/dto/PersonDTO.java
@@ -1,4 +1,37 @@
 package com.example.dutpractice1.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class PersonDTO {
+    @NotEmpty(message = "Name should be not empty!")
+    @Size(min = 2, max = 100, message = "Username should be between 2 and 100 characters")
+    private String username;
+    @NotEmpty(message = "Password should be not empty!")
+    @Size(min = 8, message = "Password must be more than 8 characters!")
+    private String password;
+
+    public PersonDTO() {
+    }
+
+    public PersonDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
